e7bd94e60d26d0cedc0e1ef117b47b375368657b
Johannes Horler <johannes.horler@dzne.de>
2023-05-29 14:43:16 +0200

    Version 0.2.0

--- src/lib_dzne_tsv/__init__.py
+++ src/lib_dzne_tsv/__init__.py
@@ -1,7 +1,9 @@
-import csv as _csv
 import collections as _collections
-import pandas as _pd
 import contextlib as _contextlib
+import csv as _csv
+
+import lib_dzne_filedata as _fd
+import pandas as _pd
 
 
 def reader(target):
@@ -173,3 +175,25 @@ class DictWriter(_DictHandler):
         self.use_handler(values)
 
 
+class TSVData(_fd.FileData):
+    _ext = '.tsv'
+    @classmethod
+    def _load(cls, /, file, *, strip=False, **kwargs):
+        ans = _tsv.read_DataFrame(file, **kwargs)
+        if strip:
+            ans = ans.applymap(lambda x: x.strip())
+        return cls(ans)
+    def _save(self, /, file, *, strip=False):
+        ans = self._dataFrame
+        if strip:
+            ans = ans.applymap(lambda x: x.strip())
+        _tsv.write_DataFrame(file, data)
+    @staticmethod
+    def _default():
+        return dict()
+    @staticmethod
+    def clone_data(data):
+        data = _pd.DataFrame(data)
+        data = data.copy()
+        data = data.applymap(str)
+        return data
